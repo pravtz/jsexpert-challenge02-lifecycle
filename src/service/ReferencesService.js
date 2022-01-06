6ee648a9119653b13edcc1af5e25522b0f425cd1
pravtz <ederson.ufrgs@gmail.com>
2022-01-06 13:12:08 -0300

    challenge ReferencesService

--- src/service/ReferencesService.js
+++ src/service/ReferencesService.js
@@ -1,21 +1,26 @@
+// const v8 = require("v8"); // soluction of the Thales
+
+
 class ReferencesService {
   getCounterCopyingReferences(object) {
-    // TODO: assing object to a new variable and return it.
-    return null;
+    const newObject = object
+    return newObject
   }
 
   getCounterWithoutCopyingReferences(object) {
-    // TODO: assing object to a new variable and return it.
-    return null;
+    // return { ...object }; // soluction of the Thales
+    let newObject = Object.create(object)
+    return newObject
   }
 
-  /*
-  // @TIP: if you want to do the extra sub-challenge, uncomment and implement the following function:
+  
   getCounterWithoutDeepCopyingReferences(object) {
-    // TODO: assing object to a new variable and return it.
-    return null;
+    //const newObject = structuredClone(object)
+    // return v8.deserialize(v8.serialize(object)); // soluction of the Thales
+    const newObject = JSON.parse(JSON.stringify(object))
+    return newObject;
   }
-  */
+  
 }
 
 module.exports = ReferencesService;
